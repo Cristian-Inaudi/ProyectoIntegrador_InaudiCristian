ca031cf5b12565ba141f95de0252060f9dd105fe
Cristian_Inaudi <crinaudi@gmail.com>
2022-06-06 12:53:08 -0300

    HEADER

---
+++ src/app/components/arg-prog/arg-prog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArgProgComponent } from './arg-prog.component';
+
+describe('ArgProgComponent', () => {
+  let component: ArgProgComponent;
+  let fixture: ComponentFixture<ArgProgComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArgProgComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ArgProgComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
